1cceb2bf84238a9f688c01f7b4b83ddd2df1cf70
czhang1234 <zhangxiao1234@hotmail.com>
2013-05-05 20:16:07 -0400

    Unit test the List action of ProductController

--- ZG.Store.Tests/Controllers/ProductControllerTest.cs
+++ ZG.Store.Tests/Controllers/ProductControllerTest.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using ZG.Domain.DTO;
 using ZG.Domain.Models;
 using ZG.Store.Application;
 using ZG.Store.Presentation.Controllers;
+using ZG.Store.Presentation.ViewModels;
 
 namespace ZG.Store.Tests.Controllers
 {
@@ -17,26 +19,29 @@ namespace ZG.Store.Tests.Controllers
         public void Can_Paginate()
         {
             //Arrange
+            var products = new[]
+                    {
+                        new Product {Id = 5, ProductName = "P 5"}, new Product {Id = 6, ProductName = "P 6"}, new Product {Id = 7, ProductName = "P 7"}
+                    }.AsQueryable();
 
-            //var mock = new Mock<IProductService>();
-            //mock.Setup(p => p.GetProducts(It.Is<int>(v => v == 2), It.Is<int>(v => v == 4))).Returns(new[]
-            //    {
-            //        new Product {Id = 5, ProductName = "P 5"},
-            //        new Product {Id = 6, ProductName = "P 6"},
-            //        new Product {Id = 7, ProductName = "P 7"}
-            //    }.AsQueryable());
+            var mock = new Mock<IProductService>();
+            mock.Setup(p => p.GetProducts(It.Is<int>(v => v == 2), It.Is<int>(v => v == 4)))
+                .Returns(new ProductsPerPage { Products = products, TotalProducts = 7 });
 
-            //var controller = new ProductController(mock.Object);
+            var controller = new ProductController(mock.Object);
 
-            ////Act
-            //var result = (IEnumerable<Product>)controller.List(2).Model;
+            //Act
+            var result = (ProductListViewModel)controller.List(2).Model;
 
-            ////Assert
-            //Product[] arrProd = result.ToArray();
-            //Assert.IsTrue(arrProd.Length == 3);
-            //Assert.AreEqual(arrProd[0].ProductName, "P 5");
-            //Assert.AreEqual(arrProd[1].ProductName, "P 6");
-            //Assert.AreEqual(arrProd[2].ProductName, "P 7");
+            //Assert
+            Product[] arrProd = result.Products.ToArray();
+            Assert.IsTrue(arrProd.Length == 3);
+            Assert.AreEqual(arrProd[0].ProductName, "P 5");
+            Assert.AreEqual(arrProd[1].ProductName, "P 6");
+            Assert.AreEqual(arrProd[2].ProductName, "P 7");
+            Assert.AreEqual(result.CurrentPageNum, 2);
+            Assert.AreEqual(result.RecordsPerPage, 4);
+            Assert.AreEqual(result.TotalPages, 2);
         }
     }
 }
