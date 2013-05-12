588609ea5dc1c2197c98743038b858079068987e
czhang1234 <zhangxiao1234@hotmail.com>
2013-05-11 22:27:15 -0400

    Unit test Cart class.

---
+++ ZG.Domain/Models/Cart.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZG.Common;
+
+namespace ZG.Domain.Models
+{
+    public class Cart
+    {
+        private List<CartLine> _cartLines = new List<CartLine>();
+
+        public IEnumerable<CartLine> Lines
+        {
+            get { return _cartLines; }
+        }
+
+        public void AddItem(Product product, int quantity)
+        {
+            UpdateItem(product, quantity, UpdateQuantityOption.Add);
+        }
+
+        public void UpdateItem(Product product, int quantity)
+        {
+            UpdateItem(product, quantity, UpdateQuantityOption.Update);
+        }
+
+        public void RemoveItem(Product product)
+        {
+            _cartLines.RemoveAll(l => l.Product.Id == product.Id);
+        }
+
+        public decimal ComputeTotalValue()
+        {
+            return _cartLines.Sum(l => l.Quantity * l.Product.Price);
+        }
+
+        public void Clear()
+        {
+            _cartLines.Clear();
+        }
+
+        public void UpdateItem(Product product, int quantity, UpdateQuantityOption updateQuantityOption)
+        {
+            CartLine line = _cartLines.FirstOrDefault(p => p.Product.Id == product.Id);
+
+            if (line == null)
+            {
+                _cartLines.Add(new CartLine { Product = product, Quantity = quantity });
+            }
+            else
+            {
+                if (updateQuantityOption == UpdateQuantityOption.Add)
+                {
+                    line.Quantity += quantity;
+                }
+                else if (updateQuantityOption == UpdateQuantityOption.Update)
+                {
+                    if (quantity > 0)
+                    {
+                        line.Quantity = quantity;
+                    }
+                    else
+                    {
+                        RemoveItem(product);
+                    }
+                }
+            }
+        }
+    }
+
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
