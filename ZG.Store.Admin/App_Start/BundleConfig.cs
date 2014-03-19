1bf5426b083cfc2d57615893a127e1e4f9ae8701
czhang1234 <zhangxiao1234@hotmail.com>
2014-03-19 02:07:35 -0400

    1

--- ZG.Store.Admin/App_Start/BundleConfig.cs
+++ ZG.Store.Admin/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace ZG.Store.Admin
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include("~/Scripts/angular.js"));
+            bundles.Add(new ScriptBundle("~/bundles/createProduct").Include("~/Scripts/App/CreateProduct.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
--- ZG.Store.Admin/Scripts/App/StoreAdminApp.js
+++ ZG.Store.Admin/Scripts/App/StoreAdminApp.js
@@ -1,30 +1,10 @@
-﻿//var phonecatApp = angular.module('phonecatApp', [
-//  'ngRoute',
-//  'phonecatControllers'
-//]);
-
-//phonecatApp.config(['$routeProvider',
-//  function ($routeProvider) {
-//      $routeProvider.
-//        when('/phones', {
-//            templateUrl: 'partials/phone-list.html',
-//            controller: 'PhoneListCtrl'
-//        }).
-//        when('/phones/:phoneId', {
-//            templateUrl: 'partials/phone-detail.html',
-//            controller: 'PhoneDetailCtrl'
-//        }).
-//        otherwise({
-//            redirectTo: '/phones'
-//        });
-//  }]);
-
-/// <reference path="../scripts/angular.js" />
+﻿/// <reference path="../scripts/angular.js" />
 
 var adminApp = angular.module('storeAdminApp', ['ngRoute', 'storeAdminControllers']);
 
 adminApp.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/???', { templateUrl: '', controller: '' })
-    .when('/???', { templateUrl: '', controller: '' })
-    .otherwise({ redirectTo: '/???' });
+    $routeProvider.when('/products/:productId', { templateUrl: 'Views/Product/Create.cshtml', controller: 'CreateProductCtrl' })
+    .when('/products', { templateUrl: 'Views/Product/List.cshtml', controller: 'ProductListCtrl' })
+    .otherwise({ redirectTo: '/products' });
 }]);
+
