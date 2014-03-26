d505861b2571ef22305788adf448cbff9ccaf364
czhang1234 <zhangxiao1234@hotmail.com>
2014-03-26 02:51:24 -0400

    1

--- ZG.Store.Admin/App/js/services.js
+++ ZG.Store.Admin/App/js/services.js
@@ -1,7 +1,6 @@
 ﻿var prodAdminServices = angular.module('prodAdminServices', ['ngResource']);
 
 prodAdminServices.factory('ProdService', ['$resource', function ($resource) {
-    //return $resource('product/list', {}, {});
     return {
         products: $resource('product/list', {}, {}),
         product: $resource('product/edit/:prodId', {}, {})
